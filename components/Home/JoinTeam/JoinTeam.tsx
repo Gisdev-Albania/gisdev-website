fe0becfe471d5e865ae4501f129b2df7deecc344
Kliv Spahiu <102724470+KlivSpahiu@users.noreply.github.com>
2022-09-30 13:56:18 +0200

    Small changes

--- components/Home/JoinTeam/JoinTeam.tsx
+++ components/Home/JoinTeam/JoinTeam.tsx
@@ -5,13 +5,8 @@ import styles from '../../../styles/components/JoinTeam.module.scss';
 export const JoinTeam = () => {
   return (
     <div className={styles.join__main__container}>
-      <div className={styles.join__section}>
-        <div className={styles.join__top__section}>
-          <h1>Don`t be Late, Join Us Today!</h1>
-        </div>
-        <div className={styles.join__bottom__section}>
-          <Button isPrimary>Join now </Button>
-        </div>
+      <div className={styles.join__top__section}>
+        <h1 className={styles.banner__text}>Don`t be Late, Join Us Today!</h1>
       </div>
     </div>
   );
---
+++ components/OurTeam/OurTeam.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../../styles/components/Header.module.scss';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const OurTeam = () => {
+  return <></>;
+};
