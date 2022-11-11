6d41169b578a06daca62d581970db0f1baa56855
Kliv Spahiu <102724470+KlivSpahiu@users.noreply.github.com>
2022-11-11 01:35:41 +0100

    NavBar grows  on scroll

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -6,44 +6,17 @@ import { MdMail } from 'react-icons/md';
 import { GrLinkedinOption } from 'react-icons/gr';
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import { useScrollPosition } from '../Header/useScrollPosition';
 
 const Layout = (props: any) => {
+
+  const scrollPosition = useScrollPosition()
+
   const { children } = props;
   return (
     <>
-      <div className={styles.navbar}>
-        <div className={styles.topbar__nav}>
-          {/*<div className={styles.topbar__content}>*/}
-          {/*  <div className={styles.topbar__left}>*/}
-          {/*    <div className={styles.topbar__left__text}>*/}
-          {/*      <IoLocationSharp />*/}
-          {/*      <h5>Tirane, Albania</h5>*/}
-          {/*    </div>*/}
-          {/*    <div className={styles.topbar__left__text}>*/}
-          {/*      <BsFillTelephoneFill />*/}
-          {/*      <h5>+355672772097</h5>*/}
-          {/*    </div>*/}
-          {/*    <div className={styles.topbar__left__text}>*/}
-          {/*      <MdMail />*/}
-          {/*      <h5>info@gis-dev.com</h5>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*  <div className={styles.topbar__right}>*/}
-          {/*    <div className={styles.topbar__info}>*/}
-          {/*      <MdMail />*/}
-          {/*    </div>*/}
-          {/*    <div className={styles.topbar__info}>*/}
-          {/*      <BsFacebook />*/}
-          {/*    </div>*/}
-          {/*    <div className={styles.topbar__info}>*/}
-          {/*      <BsTwitter />*/}
-          {/*    </div>*/}
-          {/*    <div className={styles.topbar__info}>*/}
-          {/*      <GrLinkedinOption />*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
-        </div>
+      <div className={styles.navbar} style={{marginTop: scrollPosition > 0 ? '0' : '1.5rem', transition: 'ease .3s '}}>
+        
         <Header />
       </div>
       {children}
