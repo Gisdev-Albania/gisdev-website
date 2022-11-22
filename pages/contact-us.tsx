117d41df2c7cc364d8bb65729aaf52c513dc9647
faikbinaj <102725621+faikbinaj@users.noreply.github.com>
2022-11-22 16:06:35 +0100

    Fixes/contuct us page (#14)
    * contact-us UI changes, add location to web.

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -2,16 +2,38 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import styles from '../styles/components/contactUs.module.scss';
 import Button from '../components/Button';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { TbMessageCircle2 } from 'react-icons/tb';
+import { GrMapLocation } from 'react-icons/gr';
+import { TfiMapAlt } from 'react-icons/tfi';
 
 const ContactUs = () => {
   return (
     <Layout>
       <div className={styles.navigation__section}>
         <div className={styles.container}>
-          <div className={styles.main__title}>
-            <h1>Get in touch</h1>
-          </div>
           <div className={styles.form__wrapper}>
+            <div className={styles.main__right}>
+              <div className={styles.main__title}>
+                <h1>
+                  Get in
+                  <span> touch</span>
+                </h1>
+              </div>
+              <div className={styles.main__paragraph}>
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                  dignissimos dolorem dolorum in ipsum itaque laborum maiores
+                  perspiciatis sunt tempore. Asperiores blanditiis ea earum
+                  error eveniet fugiat quas sunt voluptates!Lorem ipsum dolor
+                  sit amet, consectetur adipisicing elit. Ad dignissimos dolorem
+                  dolorum in ipsum itaque laborum maiores perspiciatis sunt
+                  tempore. Asperiores blanditiis ea earum error eveniet fugiat
+                  quas sunt voluptates!
+                </span>
+              </div>
+            </div>
+
             <div className={styles.form__main}>
               <form action="">
                 <input
@@ -57,23 +79,69 @@ const ContactUs = () => {
                 <Button isPrimary={true}>Send</Button>
               </form>
             </div>
-            <div className={styles.main__right}>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-                dignissimos dolore doloribus eos ipsa nihil odit praesentium
-                recusandae, unde. Consectetur corporis cumque dolores eos eum
-                eveniet facere laboriosam laudantium magnam, maxime quia,
-                tenetur velit voluptatem! Amet corporis eligendi laborum
-                mollitia quam tempora? Aut doloribus ipsa itaque, nemo neque
-                omnis sunt.
-              </span>
-              <div>
-                <Button isPrimary={true}>Send a request</Button>
+          </div>
+        </div>
+
+        <div className={styles.mapp__container}>
+          <div className={styles.mapp__main}>
+            <div className={styles.left__side}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.6917116248874!2d19.810431415906546!3d41.31556980840632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135031088743b727%3A0xb72c19f2ced6c110!2sGISDEV!5e0!3m2!1sen!2s!4v1668525669028!5m2!1sen!2s"
+                width="760"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className={styles.right__side}>
+              <div className={styles.right__side_text}>
+                <h1>Get In Touch With Us</h1>
+              </div>
+              <div className={styles.address}>
+                <div className={styles.row__container}>
+                  <div className={styles.container__box}>
+                    <div className={styles.icon}>
+                      <TfiMapAlt />
+                    </div>
+                    <div className={styles.info__box}>
+                      <p>
+                        Rruga Ndreko Rino, P. Viridario 1, Nr. 44, Tiranë,
+                        Albania
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div style={{ display: 'flex' }}>
+                  <div className={styles.row__container}>
+                    <div className={styles.container__box}>
+                      <div className={styles.icon}>
+                        <BsFillTelephoneFill />
+                      </div>
+                      <div className={styles.info__box}>
+                        <p>Phone: +355672772097</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={styles.row__container}>
+                    <div className={styles.container__box}>
+                      <div className={styles.icon}>
+                        <TbMessageCircle2 />
+                      </div>
+                      <div className={styles.info__box}>
+                        <p>Email: info@gis-dev.com</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>{' '}
+      </div>
     </Layout>
   );
 };
