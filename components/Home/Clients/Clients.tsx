3f17fac0224c1b5bb0edfcc3f76d5b4bc198a228
Kliv Spahiu <102724470+KlivSpahiu@users.noreply.github.com>
2023-01-10 02:02:51 +0100

    Added new slider for project section on mobile

--- components/Home/Clients/Clients.tsx
+++ components/Home/Clients/Clients.tsx
@@ -1,5 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import styles from '../../../styles/components/Clients.module.scss';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import Button from '../../Button';
 import { CardZoomOutReveal } from '../../CardZoomOutReveal';
 import { CardWrapper } from '../../CardWraper';
@@ -64,6 +72,62 @@ export const Clients = () => {
                 }
                 imageSrc={'/albanianBees.png'}
               />
+            </div>
+            <div className={styles.mobile__carousel}>
+              <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                scrollbar={{ draggable: true }}
+                onSwiper={swiper => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+              >
+                <SwiperSlide>
+                  {' '}
+                  <CardZoomOutReveal
+                    title={'Online Learning'}
+                    alt={''}
+                    description={
+                      'Some description about this card and its purpose.'
+                    }
+                    imageSrc={'/onlineLearning.png'}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  {' '}
+                  <CardZoomOutReveal
+                    title={'Dashboard'}
+                    alt={''}
+                    description={
+                      'Some description about this card and its purpose.'
+                    }
+                    imageSrc={'/myosh-bashboard.png'}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  {' '}
+                  <CardZoomOutReveal
+                    title={'Intergoma'}
+                    alt={''}
+                    description={
+                      'Some description about this card and its purpose.'
+                    }
+                    imageSrc={'/intergoma.png'}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  {' '}
+                  <CardZoomOutReveal
+                    title={'Albanian bees'}
+                    alt={''}
+                    description={
+                      'Some description about this card and its purpose.'
+                    }
+                    imageSrc={'/albanianBees.png'}
+                  />
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </div>{' '}
